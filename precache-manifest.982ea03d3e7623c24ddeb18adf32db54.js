d8027c16822bf912b56ef098a0e4c6a866b2bfe1
juan guerrero perez <juanguerreroperez@MacBook-Pro-de-juan.local>
2020-09-30 00:07:53 -0500

    Updates

---
+++ precache-manifest.982ea03d3e7623c24ddeb18adf32db54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43aa755294ca4d6867eb47279f23b710",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa75e34533e18eb6664c",
+    "url": "/static/css/main.3b713035.chunk.css"
+  },
+  {
+    "revision": "30dad2439db979e8175f",
+    "url": "/static/js/2.4de4667f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4de4667f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa75e34533e18eb6664c",
+    "url": "/static/js/main.bfab6115.chunk.js"
+  },
+  {
+    "revision": "e2ba16530f0894c493b2",
+    "url": "/static/js/runtime-main.9f16d356.js"
+  },
+  {
+    "revision": "2f929d63228457a215dc09c9cca96097",
+    "url": "/static/media/logo.2f929d63.png"
+  },
+  {
+    "revision": "1a64d253fe5c34e50056025d65a64697",
+    "url": "/static/media/whatsapp.1a64d253.png"
+  }
+]);
